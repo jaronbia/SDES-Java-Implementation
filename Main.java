059b2e8b8eca2def08fca6a7f586341313c843a4
Jaron <jaron@GailMacBookPro.local>
2019-03-13 11:29:00 -0400

    Removed modified Sbox generation, no longer needed for testing

--- Main.java
+++ Main.java
@@ -7,9 +7,5 @@ public class Main {
         SDES s1 = new SDES("1011010011", "00011101");
         s1.encryption();
         s1.decryption();
-        System.out.println("--------------------");
-        SDES s2 = new SDES("0010110011", "00111101", true);
-        s2.encryption();
-        s2.decryption();
     }
 }
